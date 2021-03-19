5b3e8a48731ed0f19d20c5f7d76233ab667f6ce0
Abraham Mendoza <abraham.mendoza@uprevenue.mx>
2021-03-19 15:13:58 -0600

    UPDATE MISION

--- pages/mision.vue
+++ pages/mision.vue
@@ -9,31 +9,20 @@
     </section>
 
     <!-- Valores -->
-    <section id="two" class="wrapper style1">
+    <section id="desktop-only" class="wrapper style1">
         <div class="container">
-            <div class="row row-mision">
-                <div class="6u">
-                  <section class="special box box-mision">
-                        <a href="#" class="image fit3"><img src="images/objetivo.png" alt="" /></a>
-                        <h3>Mision</h3>
-                        <p>Ser el proveedor de productos para el hogar más confiable y conveniente, a través del compromiso y desarrollo de nuestros colaboradores. Satisfaciendo las expectativas y necesidades de nuestros clientes.</p>
-                    </section>
-                </div>
-                <div class="6u">
-                    <section class="special box box-mision">
-                        <a href="#" class="image fit3"><img src="images/ojo.png" alt="" /></a>
-                        <h3>Vision</h3>
-                        <p>Posicionarnos como el proveedor líder en nuestro mercado a nivel norte del país, donde la logística y desarrollo de su entorno genere seguridad a sus clientes.</p>
-                    </section>
-                </div>
-            </div>
             <div class="row">
-                <div class="3u"></div>
-                 <div class="6u">
-                    <section class="special box">
-                        <a href="#" class="image fit3"><img src="images/valor.png" alt="" /></a>
-                        <h3>Valores</h3>
-                        <ul class="list-values">
+            <div class="3u">
+            <section class="special box box-mision" style="text-align:right;">
+                        <p>Ser el proveedor de productos para el hogar más confiable y conveniente, a través del compromiso y desarrollo de nuestros colaboradores. Satisfaciendo las expectativas y necesidades de nuestros clientes.</p>
+            </section>
+            </div>
+            <div class="6u" style="padding:0;">
+                <img src="/images/mision_vision.png" style="display:block;margin:auto;width=100%;"/>
+            </div>
+            <div class="3u" style="padding:0;text-align:left;">
+            <section class="special box">
+                        <ul class="list-values" style="float:left;text-align:left;">
                             <li>Servicio</li>
                             <li>Confianza</li>
                             <li>Responsabilidad</li>
@@ -41,8 +30,45 @@
                             <li>Productividad</li>
                             <li>Innovación</li>
                         </ul>
+            </section>
+            </div>
+            </div>
+            <div class="row">
+                <div class="3u"></div>
+                 <div class="6u" style="padding:0;">
+                    <section class="special box" style="width:50%;margin:auto;">
+                        <p>Posicionarnos como el proveedor líder en nuestro mercado a nivel norte del país, donde la logística y desarrollo de su entorno genere seguridad a sus clientes.</p>
                     </section>
                 </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="mobile-only" class="wrapper style1">
+        <div class="container">
+            <div class="row">
+            <div class="6u" style="padding:0px">
+            <img src="/images/mision_vision.png" style="max-width:100%;margin:auto;display:block;"/>
+            <section class="special box">
+                        <h2>Mision</h2>
+                        <p>Ser el proveedor de productos para el hogar más confiable y conveniente, a través del compromiso y desarrollo de nuestros colaboradores. Satisfaciendo las expectativas y necesidades de nuestros clientes.</p>
+            </section>
+            <section class="special box">
+                        <h2>Vision</h2>
+                        <p>Posicionarnos como el proveedor líder en nuestro mercado a nivel norte del país, donde la logística y desarrollo de su entorno genere seguridad a sus clientes.</p>
+            </section>
+            <section class="special box">
+                        <h2>Valores</h2>
+                        <ul class="list-values" style="list-style:none;padding:0">
+                            <li>Servicio</li>
+                            <li>Confianza</li>
+                            <li>Responsabilidad</li>
+                            <li>Trabajo en equipo</li>
+                            <li>Productividad</li>
+                            <li>Innovación</li>
+                        </ul>
+            </section>
+            </div>
             </div>
         </div>
     </section>
