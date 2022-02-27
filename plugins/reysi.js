f0a253e4d586d063120457227033a035124f5205
RogelioRa17 <t-rjinestas@itesm.mx>
2022-02-27 12:37:15 -0600

    FIX BUG NAV

--- plugins/reysi.js
+++ plugins/reysi.js
@@ -3,6 +3,6 @@ export default () => {
   body.setAttribute('id', 'top')
   var nav = document.getElementById('navPanel')
   if (window.screen.width < 800) {
-    nav.innerHTML = '<div data-action="moveElement" data-args="nav"><div id="skel-layers-placeholder-nav"></div><nav id="nav" class="skel-layers-moved"><ul><li><a href="/" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">Inicio</a></li> <li><a href="/nosotros" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">Nosotros</a></li> <li><a href="/catalogo" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">Catálogo</a></li> <li><a href="/contacto" class="button special" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">Contacto</a></li></ul></nav></div>';
+    nav.innerHTML = '<div data-action="moveElement" data-args="nav"><div id="skel-layers-placeholder-nav"></div><nav id="nav" class="skel-layers-moved"><ul><li><a href="/" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">Inicio</a></li> <li><a href="/nosotros" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">Nosotros</a></li> <li><a href="/catalogo" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">Catálogo</a></li> <li><a href="/contacto" class="button special" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">Contacto</a></li></ul></nav></div>'
   }
 }
